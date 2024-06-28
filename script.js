7b33149bec6109aed5f5f27241178d650cee7fab
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-06-28 16:45:28 -0300

    new features and fetch code js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}s&lang=pt_br&appid=${apiChave}&units=metric `;
+//const apiURL = ;
 const apiChave = '6b754e23fe158f87a4fd15c392fcbba9';
-var cidade = document.querySelector('#citySearch');
+var cidade = 'Londres' ;
 
 function obterTemperatura () {
-    fetch (apiURL)
+    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&lang=pt_br&appid=${apiChave}&units=metric `)
     .then (response => response.json())
     .then (data => {
         const temperatura = data.main.temp;
         const cidade = data.name;
+        const descricao = data.weather[0].description;
         const tempElement = document.getElementById('temp');
-        tempElement.innerHTML = ` A temperatura em ${cidade} é ${temperatura} °C`;
+        const cidadeElement = document.getElementById('city');
+        const descricaoElement = document.getElementById('description');
+        tempElement.innerHTML = ` ${temperatura} °C`;
+        cidadeElement.innerHTML = ` ${cidade}`;
+        descricaoElement.innerHTML = ` ${descricao}`;
     })
     .catch (error => {
         console.error('Erro ao buscar temperatura',error);
@@ -18,3 +23,5 @@ function obterTemperatura () {
 }
 
 //const apiURL = 'https://api.themoviedb.org/3/discover/movie?' + 'api_key=' + apiChave + '&language=pt-BR';
+
+//document.querySelector('#citySearch')
